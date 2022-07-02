916f6c1bde1186ce9e6a1575c6afba9afe35ba01
Ragav1196 <ragavinrap@gmail.com>
2022-07-03 02:13:06 +0530

    Commited

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,55 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /* 
+  1 -> STYLE BINDING:
+    Style binding is used to apply inlint style to the html element
+  */
   selector: 'app-test',
   template: `<h2>Welcome {{ name }}</h2>
-    <!-- 1 -> CLASS BINDING -->
-    <h2 class="text-success">RAGAV</h2>
-    <!-- Class applied using attribute -->
+    <h2 [style.color]="'orange'">STYLE BINDING</h2>
+    <!-- Binding orange color to the color property -->
 
-    <h2 [class]="successClass">RAGAV</h2>
-    <!-- Class applied using class binding method -->
+    <h2 [style.color]="hasError ? 'red' : 'orange'">STYLE BINDING</h2>
+    <!-- Binding orange or red color to the color property conditionally -->
 
-    <h2 class="text-special" [class]="successClass">RAGAV</h2>
-    <!-- using both class attribute and class binding -->
+    <h2 [style.color]="highLightColor">STYLE BINDING</h2>
+    <!-- Binding green color to the color property using class property -->
 
-    <h2
-      [class.text-danger]${
-        /* Mentioning the class which we want to apply conditionally */ ''
-      }="hasError"
-    >
-      RAGAV
-    </h2>
-    <!-- applying class binding conditionally -->
-
-    <!-- 
-    2 -> WHAT IF WE WANT TO APPLY MULTIPLE CLASS CONDITIONALY:
-        Then we can use ngClass directive. a directive is nothing but a angular custom attribute
-     -->
-    <h2 [ngClass]="messageClasses">RAGAV</h2>`,
-  styles: [
-    `
-      .text-success {
-        color: green;
-      }
-      .text-danger {
-        color: red;
-      }
-      .text-special {
-        font-style: italic;
-      }
-    `,
-  ],
+    <h2 [ngStyle]="titleStyles">STYLE BINDING</h2>
+    <!-- Binding multiple inline style using ngStyle directive -->`,
+  styles: [],
 })
 export class TestComponent implements OnInit {
   public name = 'RAGAV';
-  public successClass = 'text-success';
-  public hasError = false;
-  public isSpecial = true;
-  public messageClasses = {
-    'text-success': !this.hasError,
-    'text-danger': this.hasError,
-    'text-special': this.isSpecial,
+  public hasError = true;
+  public highLightColor = 'green';
+  public titleStyles = {
+    color: 'blue',
+    fontStyle: 'italic',
   };
   constructor() {}
 
