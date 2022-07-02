547a54d72ffc1cf2cb7f0e26fe67a4117590075b
Ragav1196 <ragavinrap@gmail.com>
2022-07-03 02:00:34 +0530

    Commited

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,56 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<h2>Welcome {{ name }}</h2>
+    <!-- 1 -> CLASS BINDING -->
+    <h2 class="text-success">RAGAV</h2>
+    <!-- Class applied using attribute -->
+
+    <h2 [class]="successClass">RAGAV</h2>
+    <!-- Class applied using class binding method -->
+
+    <h2 class="text-special" [class]="successClass">RAGAV</h2>
+    <!-- using both class attribute and class binding -->
+
+    <h2
+      [class.text-danger]${
+        /* Mentioning the class which we want to apply conditionally */ ''
+      }="hasError"
+    >
+      RAGAV
+    </h2>
+    <!-- applying class binding conditionally -->
+
+    <!-- 
+    2 -> WHAT IF WE WANT TO APPLY MULTIPLE CLASS CONDITIONALY:
+        Then we can use ngClass directive. a directive is nothing but a angular custom attribute
+     -->
+    <h2 [ngClass]="messageClasses">RAGAV</h2>`,
+  styles: [
+    `
+      .text-success {
+        color: green;
+      }
+      .text-danger {
+        color: red;
+      }
+      .text-special {
+        font-style: italic;
+      }
+    `,
+  ],
 })
 export class TestComponent implements OnInit {
+  public name = 'RAGAV';
+  public successClass = 'text-success';
+  public hasError = false;
+  public isSpecial = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial,
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
