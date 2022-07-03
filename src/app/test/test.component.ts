6e03e225699c75bdad28d288a441b24e78621bb0
Ragav1196 <ragavinrap@gmail.com>
2022-07-03 13:09:49 +0530

    Commited

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,7 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h2>Welcome {{ name }}</h2> `,
+  template: `<h2>Welcome {{ name }}</h2>
+    <!-- TEMPLATE REFERENCE VARIABLE -->
+    <input
+      #myInput
+      ${
+        /* We are providing a reference. so that we can use this reference in events like on click and access this element property */ ``
+      }
+      type="text"
+    />
+    <button (click)="logMessage(myInput)">Log 1</button>
+    <!-- Accessing the element using the reference -->
+
+    <button (click)="logMessage(myInput.value)">Log 2</button>
+    <!-- using onClick event we are accessing the input element property -->`,
   styles: [],
 })
 export class TestComponent implements OnInit {
@@ -11,4 +24,8 @@ export class TestComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  logMessage(value: any) {
+    console.log(value);
+  }
 }
