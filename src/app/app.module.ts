4c6e5219084dd2e8c1dceb7d4a4ff41f21893ad3
Ragav1196 <ragavinrap@gmail.com>
2022-07-03 13:24:24 +0530

    Commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; /* 1 -> import forms module as the ngModal directrive is part of the FormsModule */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
+  declarations: [AppComponent, TestComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule /* 1 -> we need to import that here to make angular aware of the module */,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /* 1 -> Here the data will flow from view (template) to the modal(class) and vice versa */
   selector: 'app-test',
-  template: `<h2>Welcome {{ name }}</h2> `,
+  template: `<input [(ngModel)]="name" type="text" />
+    {{ name }}
+    <!-- What we want is when user input something then that should change this name property in 
+    the class and inturn the class property name should also update the value in the view -->`,
   styles: [],
 })
 export class TestComponent implements OnInit {
-  public name = 'RAGAV';
+  public name = '';
 
   constructor() {}
 
