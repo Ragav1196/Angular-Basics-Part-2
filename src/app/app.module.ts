f15720f46ed3a3db76c3de01ceb5b4e4883772e2
Ragav1196 <ragavinrap@gmail.com>
2022-08-14 01:29:46 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
@@ -15,4 +16,11 @@ export class AppModule {}
 1 -> Two types of approach for forms 
   1 - Template Driven forms - Heavy on component template
   2 - Reactive forms - heavy on component class
+
+    Template Driven forms:
+    
+      1 -> Introduction
+        We use two way data binding with NgModule.
+        Readabilty of the form increases with complex forms
+        Suitable for simple forms
 */
